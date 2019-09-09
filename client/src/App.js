f1adc90a4b86f2eb28fd5f0df4b366e894507aed
Your name <a.m.verdine@gmail.com>
2019-09-09 17:39:23 -0400

    Refactor JSX in Movie and <MovieDetails />

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieCard from "./Movies/MovieCard";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -17,7 +18,11 @@ const App = () => {
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
       {/* <Route path="/movies/movie/:id" render={props => <Movie {...props} addToSavedList={addToSavedList} /> } /> */}
-      <Route path="/movie/:id" component={Movie} />
+      {/* <Route path="/movie/:id" component={Movie} /> */}
+      <Route path="/movie/:id" render={props => <MovieList {...props}  MovieList={MovieList} /> } />
+      <MovieCard />
+
+      {/* <MovieCard title={savedList.title} director={savedList.director} metascore={savedList.metascore}/> */}
 
     </div>
   );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const MovieCard = props => {
-  return;
+  return (
+    <>
+      <div className="movie-card">
+        <h2>{props.title}</h2>
+      </div>
+
+      <div className="movie-director">
+        Director: <em>{props.director}</em>
+      </div>
+
+      <div className="movie-metascore">
+        Metascore: <strong>{props.metascore}</strong>
+      </div>
+
+      <div>
+        <h3>Actors</h3>
+      </div>
+    </>
+  );
 };
 
 export default MovieCard;
